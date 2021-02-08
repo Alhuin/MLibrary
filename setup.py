b550635c897cf158a5583e6f8bf705438657761a
j.janin-reynaud <julien.janin-reynaud@itsfactory.fr>
2021-02-08 11:42:05 +0100

    v.0.1.1 setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='MLibrary',
     packages=find_packages(include=['MLibrary']),
-    version='0.1.1',
-    description='Machine Learning Python Library',
-    author='Alhuin',
-    license='MIT',
     install_requires=['numpy'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
-    download_url='https://github.com/Alhuin/MLibrary/archive/0.1.0.tar.gz',
 )

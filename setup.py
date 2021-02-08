203c46224f32c6fece823d38c4d7fb8993a0a015
j.janin-reynaud <julien.janin-reynaud@itsfactory.fr>
2021-02-08 10:57:37 +0100

    0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='MLibrary',
     packages=find_packages(include=['MLibrary']),
-    version='0.1.0',
+    version='0.1.1',
     description='Machine Learning Python Library',
     author='Alhuin',
     license='MIT',

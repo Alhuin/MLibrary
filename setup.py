74c787a6a17467bd55a054e2b54920743132a952
j.janin-reynaud <julien.janin-reynaud@itsfactory.fr>
2021-02-08 10:56:13 +0100

    decription-file in setup.cfg download_url in setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
+    download_url='https://github.com/Alhuin/MLibrary/archive/0.1.0.tar.gz',
 )
